7185f86e8398afcb81748ab94544ddefb2ad563d
paulbourelly999 <paul.k.bourelly@leidos.com>
2025-04-07 11:37:17 -0400

    Updates

--- test/TestUdpSocket.cpp
+++ test/TestUdpSocket.cpp
@@ -18,4 +18,19 @@ TEST( testUdpSocket, testUdpSocketClientServer) {
 
 }
 
+TEST( testUdpSocket, testUdpSocketClientServerDebug) {
+    udp_socket::UdpClient client("127.0.0.1", 5832, true);
+    udp_socket::UdpServer server("127.0.0.1", 5832, true);
+    std::string testMsg = "Testing";
+    client.Send(testMsg);
+    auto msg =  server.stringTimedReceive(100);
+    EXPECT_EQ(testMsg, msg);
+    EXPECT_EQ(client.GetAddress(), "127.0.0.1");
+    EXPECT_EQ(client.GetPort(), 5832);  
+    testMsg = "1Testing";
+    client.Send(testMsg);
+    msg =  server.stringTimedReceive(100);
+    EXPECT_EQ(testMsg, msg);
+}
+
 
