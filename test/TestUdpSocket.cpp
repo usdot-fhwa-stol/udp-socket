07082cf031612367b9f36b85e0e952b77fc7ce82
paulbourelly999 <77466294+paulbourelly999@users.noreply.github.com>
2025-04-18 10:31:03 -0400

    Fix debug logging (#2)
    * Fix debug logging
    
    * Updates
    
    * Updates

--- test/TestUdpSocket.cpp
+++ test/TestUdpSocket.cpp
@@ -10,9 +10,27 @@ TEST( testUdpSocket, testUdpSocketClientServer) {
     auto msg =  server.stringTimedReceive(100);
     EXPECT_EQ(testMsg, msg);
     EXPECT_EQ(client.GetAddress(), "127.0.0.1");
-    EXPECT_EQ(client.GetPort(), 5832);
-    EXPECT_EQ(server.GetAddress(), "127.0.0.1");
-    EXPECT_EQ(server.GetPort(), 5832);  
+    EXPECT_EQ(client.GetPort(), 5832);  
+    testMsg = "1Testing";
+    client.Send(testMsg);
+    msg =  server.stringTimedReceive(100);
+    EXPECT_EQ(testMsg, msg);
+
+}
+
+TEST( testUdpSocket, testUdpSocketClientServerDebug) {
+    udp_socket::UdpClient client("127.0.0.1", 5832, true);
+    udp_socket::UdpServer server("127.0.0.1", 5832, true);
+    std::string testMsg = "Testing";
+    client.Send(testMsg);
+    auto msg =  server.stringTimedReceive(100);
+    EXPECT_EQ(testMsg, msg);
+    EXPECT_EQ(client.GetAddress(), "127.0.0.1");
+    EXPECT_EQ(client.GetPort(), 5832);  
+    testMsg = "1Testing";
+    client.Send(testMsg);
+    msg =  server.stringTimedReceive(100);
+    EXPECT_EQ(testMsg, msg);
 }
 
 
